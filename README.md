2fd44f407fa580dfbdfef3b41579e65eac2f62e3
liuyiyou <liuyiyou@daojia.com>
2017-11-16 14:52:45 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,9 @@
+## 一、项目结构
+[SpringClound学习](http:/liuyiyou.cn/springcloud)
+
+#### a. 『 基础 - 入门篇 』
+- eureka<br>
+  [《SpringCloud之最基础的服务中心》](http://liuyiyou/SpringCloud之最基础的服务中心 "SpringCloud之最基础的服务中心")<br>
+- hello-service <br>
+ [《SpringCloud之最基础的服务》](http://liuyiyou/SpringCloud之最基础的服务 "SpringCloud之最基础的服务")<br>
+
---
+++ hello-service/src/main/java/cn/liuyiou/cloud/controller/HelloController.java
@@ -0,0 +1,30 @@
+package cn.liuyiou.cloud.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User: liuyiyou
+ * Date: 2017/9/21
+ * Time: 下午1:34
+ */
+@RestController
+public class HelloController {
+
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @RequestMapping("/hello")
+    public String index(){
+        ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+        logger.info("/hello,host:"+instance.getHost()+",service_id:"+instance.getServiceId());
+        return "Hello Spring Cloud";
+    }
+}

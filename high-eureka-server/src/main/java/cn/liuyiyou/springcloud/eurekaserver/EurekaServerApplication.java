c47d2cacbbfbc9bd81e46640c53299342aaf27f9
liuyiyou <liuyiyou@daojia.com>
2017-11-17 10:55:09 +0800

    新增了高可用服务注册中心 新增服务消费方 新增负载均衡

---
+++ high-eureka-server/src/main/java/cn/liuyiyou/springcloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package cn.liuyiyou.springcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * 该注解启动一个服务注册中心给其他应用进行对话
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
